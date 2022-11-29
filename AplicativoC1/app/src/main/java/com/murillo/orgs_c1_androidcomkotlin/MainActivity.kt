be38ef8f4a3c7f680a568ac5dba741b67e9476ab
LoxiasMegalos <murillo.alkantara@gmail.com>
2022-11-29 15:01:33 -0300

    Adicionando aplicativo curso 1

---
+++ AplicativoC1/app/src/main/java/com/murillo/orgs_c1_androidcomkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.murillo.orgs_c1_androidcomkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
